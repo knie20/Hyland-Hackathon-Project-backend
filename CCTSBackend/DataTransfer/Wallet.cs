34965bd440f1e78482992f1cf1159182ed3501af
Greshbolt <greshbolt@gmail.com>
2017-12-16 12:44:15 -0500

    Created Transaction and User models

--- CCTSBackend/DataTransfer/Wallet.cs
+++ CCTSBackend/DataTransfer/Wallet.cs
@@ -9,11 +9,8 @@ namespace CCTSBackend.DataTransfer
     {
         public virtual long userID {get; set;}
         public virtual double amount {get; set;}
-
         public virtual string pubKey {get; set;}
-
         public Wallet() { }
-        
         public Wallet(long user, double amount, string pubKey){
             this.userID = user;
             this.amount = amount;
