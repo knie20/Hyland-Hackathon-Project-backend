7c59e17a156b748b27dbfd3ea08fc6014dfc7716
Kaidong Nie <niekg@mail.uc.edu>
2017-12-17 08:30:17 -0500

    f

--- CCTSBackend/Startup.cs
+++ CCTSBackend/Startup.cs
@@ -17,7 +17,6 @@ namespace CCTSBackend
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            BlockCypherApiCaller.GenerateWallet();
         }
 
         public IConfiguration Configuration { get; }
